d29dbdda70672f122e9584007e6e8c1b996c8f44
Graham Hughes <graham@rethinkdb.com>
2014-07-16 15:17:14 -0700

    Replace old read_t serialization, as we didn't need that field.

--- src/rdb_protocol/protocol.cc
+++ src/rdb_protocol/protocol.cc
@@ -1054,10 +1054,7 @@ RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(sindex_status_t, sindexes, region);
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(changefeed_subscribe_t, addr, region);
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(changefeed_stamp_t, addr, region);
 
-// Serialization format changed in 1.14.0. We only support the latest version,
-// since this is a cluster-only type.
-RDB_IMPL_SERIALIZABLE_2(read_t, read, profile);
-INSTANTIATE_SERIALIZABLE_FOR_CLUSTER(read_t);
+RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(read_t, read, profile);
 
 RDB_IMPL_SERIALIZABLE_1_SINCE_v1_13(point_write_response_t, result);
 RDB_IMPL_SERIALIZABLE_1_SINCE_v1_13(point_delete_response_t, result);
